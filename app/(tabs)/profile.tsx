62bbe6c94891b40363ee179dc6c142d6a4c62f06
Marvin Ambrose <marvambi@gmail.com>
2023-08-15 20:13:24 +0100

    docs: updated profile.tsx

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,23 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Text, View } from "react-native";
+import { GluestackUIProvider, Text, Box, config } from "@gluestack-ui/react";
 
-export default function App() {
+export default function Profile() {
+	
 	return (
-		<View className={"flex-1 items-center justify-center bg-yellow-200"}>
-			<Text>Open up profile.tsx to start working on your app!</Text>
-			<StatusBar style="auto" />
-		</View>
+		<GluestackUIProvider config={config.theme}>
+			<Box minWidth="80%" justifyContent="center" alignItems="center" backgroundColor="cyan" borderTopLeftRadius={20} borderTopRightRadius={20} marginRight={5} marginLeft={5}>
+				<Text size={"2xl"} padding={2} bold={"true"}>Open up profile.tsx to start working on this component!</Text>
+				<Text>Difficult</Text>
+			</Box>
+		</GluestackUIProvider>
 	);
 }
+
+// export default function App() {
+// 	return (
+// 		<View className={"flex-1 items-center justify-center bg-yellow-200"}>
+// 			<Text>Open up profile.tsx to start working on your app!</Text>
+// 			<StatusBar style="auto" />
+// 		</View>
+// 	);
+// }
