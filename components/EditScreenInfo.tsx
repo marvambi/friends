32201ccd087864f52db3a400fb6337b911c8452a
Marvin Ambrose <marvambi@gmail.com>
2023-08-19 20:47:38 +0100

    docs: configured indentation from spaces to tabs

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -1,77 +1,77 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React from "react";
+import { StyleSheet } from "react-native";
 
-import Colors from '../constants/Colors';
-import { ExternalLink } from './ExternalLink';
-import { MonoText } from './StyledText';
-import { Text, View } from './Themed';
+import Colors from "../constants/Colors";
+import { ExternalLink } from "./ExternalLink";
+import { MonoText } from "./StyledText";
+import { Text, View } from "./Themed";
 
 
 export default function EditScreenInfo({ path }: { path: string }) {
-  return (
-    <View>
-      <View style={styles.getStartedContainer}>
-        <Text
-          style={styles.getStartedText}
-          lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)">
-          Open up the code for this screen:
-        </Text>
+	return (
+		<View>
+			<View style={styles.getStartedContainer}>
+				<Text
+					style={styles.getStartedText}
+					lightColor="rgba(0,0,0,0.8)"
+					darkColor="rgba(255,255,255,0.8)">
+					Open up the code for this screen:
+				</Text>
 
-        <View
-          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
-          darkColor="rgba(255,255,255,0.05)"
-          lightColor="rgba(0,0,0,0.05)">
-          <MonoText>{path}</MonoText>
-        </View>
+				<View
+					style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
+					darkColor="rgba(255,255,255,0.05)"
+					lightColor="rgba(0,0,0,0.05)">
+					<MonoText>{path}</MonoText>
+				</View>
 
-        <Text
-          style={styles.getStartedText}
-          lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)">
-          Change and customize any of the configuration, and your profile will automatically update.
-        </Text>
-      </View>
+				<Text
+					style={styles.getStartedText}
+					lightColor="rgba(0,0,0,0.8)"
+					darkColor="rgba(255,255,255,0.8)">
+					Change and customize any of the configuration, and your profile will automatically update.
+				</Text>
+			</View>
 
-      <View style={styles.helpContainer}>
-        <ExternalLink
-          style={styles.helpLink}
-          href="https://marvambi.com">
-          <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
-            Tap here to get more depth &amp; information about the app 
-          </Text>
-        </ExternalLink>
-      </View>
-    </View>
-  );
+			<View style={styles.helpContainer}>
+				<ExternalLink
+					style={styles.helpLink}
+					href="https://marvambi.com">
+					<Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
+						Tap here to get more depth &amp; information about the app 
+					</Text>
+				</ExternalLink>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightContainer: {
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  helpContainer: {
-    marginTop: 15,
-    marginHorizontal: 20,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    textAlign: 'center',
-  },
+	getStartedContainer: {
+		alignItems: "center",
+		marginHorizontal: 50,
+	},
+	homeScreenFilename: {
+		marginVertical: 7,
+	},
+	codeHighlightContainer: {
+		borderRadius: 3,
+		paddingHorizontal: 4,
+	},
+	getStartedText: {
+		fontSize: 17,
+		lineHeight: 24,
+		textAlign: "center",
+	},
+	helpContainer: {
+		marginTop: 15,
+		marginHorizontal: 20,
+		alignItems: "center",
+	},
+	helpLink: {
+		paddingVertical: 15,
+	},
+	helpLinkText: {
+		textAlign: "center",
+	},
 });
